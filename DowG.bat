3f477774de0c3622972536787b137b77bf289533
Reskilee <112638623+Reskilee@users.noreply.github.com>
2024-07-04 23:28:22 +0900

    Delete DowG.bat

--- DowG.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-curl -L -O https://github.com/Reskilee/CFGCS2/archive/refs/heads/main.zip
-echo Expand-Archive -Force -LiteralPath main.zip > execute.ps1
-powershell -executionpolicy RemoteSigned -file execute.ps1 
-move main\CFG-main\kali.ps1 .\
-xcopy main\CFG-main\* "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\" /Y
-rmdir main /S /Q
-echo DEL DowG.bat DowG.zip DeleteAll.bat main.zip execute.ps1 kali.ps1 > DeleteAll.bat
